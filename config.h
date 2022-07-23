0902b97d9cdef7864d05229e68b0fe7ba5608de8
Mishalabdullah <github.ijvob@simplelogin.co>
2022-07-23 19:43:20 +0530

    added brightness controls

--- config.h
+++ config.h
@@ -63,8 +63,8 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "gnome-terminal", NULL };
 static const char *slockcmd[] = { "slock", NULL };
-static const char *brightnessup[] = {"light", "-A", "10", NULL };
-static const char *brightnessdown[] = {"light", "-U", "10", NULL };
+static const char *brightnessup[] = { "xbacklight", "-", "10", NULL };
+static const char *brightnessdown[] = { "xbacklight", "+", "10", NULL };
 /*volume */
 static Key keys[] = {
 	/* modifier                     key        function        argument */
